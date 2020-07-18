40bc22ea543a4b031a662193f849d03d11fd8192
onting <hemool123@naver.com>
2020-07-18 17:51:28 +0900

    MainView 작성

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, Text, TouchableHighlight } from 'react-native';
+import LoginView from './compo/loginview';
+
+const MainView = () => {
+  const [state, setState] = React.useState('feed');
+
+  return(
+    <View style={{
+      flex: 1,
+      flexDirection: 'column',
+    }}>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>state: {state}</Text>
+      </View>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginVertical: 20,
+      }}>
+        <TouchableHighlight onPress={() => setState('feed')}>
+          <Image source={require('./assets/favicon.png')} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => setState('org')}>
+          <Image source={require('./assets/favicon.png')} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => setState('notice')}>
+          <Image source={require('./assets/favicon.png')} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => setState('profile')}>
+          <Image source={require('./assets/favicon.png')} />
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
+  const target = AsyncStorage.getItem('username') ? <LoginView /> : <MainView />;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!ppppp</Text>
+      {target}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +57,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
