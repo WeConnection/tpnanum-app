1d76c7944f2f5a0508f70b7baac878faeb67fc2a
onting <hemool123@naver.com>
2020-08-04 13:25:58 +0900

    Write icon view

--- compo/orgview.js
+++ compo/orgview.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, TouchableHighlight } from 'react-native';
 import hostaddr from '../config'
+
+const OrgIcon = (props) => {
+    return (
+        <TouchableHighlight>
+            <Image source={{uri: props.imageUri}} />
+            <Text>
+                {props.name}
+            </Text>
+            <Text>
+                {props.tags[0]}
+            </Text>
+        </TouchableHighlight>
+    );
+}
 
 const OrgView = () => {
     return (
