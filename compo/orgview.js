ad7f95468c77e806fad87ca9657e02ec1f75792e
onting <hemool123@naver.com>
2020-08-10 17:59:01 +0900

    OrgView 서버와 연동

--- compo/orgview.js
+++ compo/orgview.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { ScrollView, Text, View, Image, TouchableHighlight, useWindowDimensions } from 'react-native';
-import hostaddr from '../config'
+import AsyncStorage from '@react-native-community/async-storage';
+import React, {useState, useEffect} from 'react';
+import { ScrollView, Text, View, Image, TouchableHighlight, useWindowDimensions, Alert } from 'react-native';
+import {hostaddr} from '../config'
 
 const OrgIcon = (props) => {
     return (
-        <View>
+        <View style={props.style}>
             <TouchableHighlight>
-                <Image source={require('../assets/favicon.png')} />
+                <Image source={require('../assets/favicon.png')} style={{
+                    height: 60,
+                    width: 60,
+                    resizeMode: 'cover',
+                }} />
             </TouchableHighlight>
             <Text>
-                이름
-            </Text>
-            <Text>
-                테그, 테그
+                {props.name}
             </Text>
         </View>
     );
@@ -20,31 +22,69 @@ const OrgIcon = (props) => {
 
 const OrgView = () => {
     const width = useWindowDimensions().width;
-    const count = 10;
     const fit = parseInt(width / 120);
-    let contents = [];
+    const [contents, setContents] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    for(let i = 0; i < count / fit; i++)
-    {
-        let row = [];
-        for(let j = 0; (j < fit) && (i * fit + j < count); j++)
+    const getContents = (orgs) => {
+        const count = orgs.length;
+        let contents = [];
+
+        for(let i = 0; i < count / fit; i++)
         {
-            row.push(<OrgIcon />);
+            let row = [];
+            for(let j = 0; (j < fit) && (i * fit + j < count); j++)
+            {
+                row.push(<OrgIcon key={j.toString()} name={orgs[i * fit + j].name} tags={orgs[i * fit + j].tags} style={{
+                    marginHorizontal: 20,
+                    alignItems: 'center',
+                }} />);
+            }
+            
+            contents.push(
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginHorizontal: 5,
+                    marginVertical: 20,
+                }} key={i.toString()}>
+                    {row}
+                </View>
+            );
         }
         
-        contents.push(
-            <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-                alignItems: 'center',
-                marginHorizontal: 10,
-                marginVertical: 20,
-            }}>
-                {row}
-            </View>
-        );
-    }
+        return contents;
+    };
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(hostaddr + '/org/list', {
+            method: 'GET',
+        })
+        .then(res => res.json())
+        .then(json => {
+            const orgs = json['orgs'];
+            setContents(getContents(orgs));
+        })
+        .catch(err => {
+            Alert.alert(
+            '세션 만료',
+            String(err),
+            [
+              {
+                text: '닫기',
+                style: 'cancle',
+              },
+            ],
+            {cancelable: false},
+            );
+            AsyncStorage.setItem('username', '').then(() => navigation.navigate('Login'));
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+    }, []);
 
     return (
         <ScrollView style={{
@@ -53,7 +93,7 @@ const OrgView = () => {
             marginHorizontal: 20,
             marginVertical: 10,
         }}>
-            {contents}
+            {loading ? <Text>로딩중</Text> : contents}
         </ScrollView>
     );
 };
